4b522e16d8005371977dab01ff1bb4fdd319475a
xaquingv <xocasgv@gmail.com>
2021-05-15 18:05:36 +0200

    TWEAK size of Blanket

--- app/src/components/Blanket.svelte
+++ app/src/components/Blanket.svelte
@@ -6,28 +6,15 @@
     import { Canvas } from 'svelte-canvas'
 	import {scaleSqrt, scaleLinear} from 'd3-scale';
 	import {extent} from 'd3-array';
-    import Grid from './Grid.svelte'
+    import Grid from './Grid.svelte';
 
     export let data;
     let margin = {top: 20, right: 10, bottom: 20, left: 125};
-	  let width, height;
-    let frame;
-
-
-    // let dataMap = data.reduce((acc,d) => {
-    //   acc[d.cy2 - 1] = acc[d.cy2 - 1] ?? [];
-    //   acc[d.cy2 - 1][d.cx - 1] = d;
-    //   return acc;
-    // }, [])
-
-    let tooltipPosition = [-1,-1];
+	let width, height;
 
     $: x = scaleLinear()
 		.domain(extent(data, d => d.cx))
 		.range([margin.left + 10, width - margin.right - margin.right]);
-
-    // $: stepX = (x.range()[1] - x.range()[0]) / dataMap[0].length;
-    // $: stepY = (y.range()[1] - y.range()[0]) / dataMap.length;
 
 	$: y = scaleLinear()
 		.domain(extent(data, d => d.cy2))
@@ -43,45 +30,17 @@
 		.range([0, 15])
 		.nice();
 
-    function updateTooltip(x,y) {
-      console.log(dataMap[y][x]);
-    }
-
-    function handleMousemove(event) {
-      const row = Math.min(Math.max(0, Math.floor((event.clientY - (margin.top)) / stepY)), dataMap.length - 1);
-      const col = Math.min(Math.max(0, Math.floor((event.clientX - (margin.left + 10)) / stepX)), dataMap[row].length - 1);
-
-      // console.log(col,row,'->');
-      // console.log(dataMap[row][col])
-      if(tooltipPosition.col !== col || tooltipPosition.row !== row) {
-        updateTooltip(col, row);
-      }
-
-  	}
-    // LET'S USE THIS LATER TO IMPROVE THE PERFORMANCE
-    function handleMouseenter() {
-      console.log('ENTER');
-      // (function loop() {
-  		// 	frame = requestAnimationFrame(loop);
-  		// 	// console.log(tooltipPosition);
-  		// })();
-    }
-    function handleMouseleave() {
-      console.log('LEAVE');
-      // cancelAnimationFrame(frame);
-    }
-
 </script>
-<div class='graphic overview' bind:clientWidth={width} bind:clientHeight={height}>
-    <Canvas {width} {height} on:mousemove={handleMousemove} on:mouseenter={handleMouseenter} on:mouseleave={handleMouseleave}>
-        <Grid type="x" scale={x} {data} {margin} />
-        <Grid type="y" scale={y} {data} {margin} />
+<div class='graphic square' bind:clientWidth={width} bind:clientHeight={height}>
+    <Canvas {width} {height}>
+        <!-- <Grid type="x" scale={x} {data} {margin} />
+        <Grid type="y" scale={y} {data} {margin} /> -->
             {#each data as d,i}
             {#if i%2}
             <Ellipse
                 x={x(d.cx)}
-                y={y(d.cy2)}
-                i={d.cy2}
+                y={y(d.cy)}
+                i={d.cy}
                 rx={rx(d.rminorR)}
                 ry={ry(d.rmajorR)}
                 fill='#F26680'
@@ -89,8 +48,8 @@
             />
             <Ellipse
                 x={x(d.cx)}
-                y={y(d.cy2)}
-                i={d.cy2}
+                y={y(d.cy)}
+                i={d.cy}
                 rx={rx(d.rminor)}
                 ry={ry(d.rmajor)}
                 fill='#13B2ED'
@@ -99,8 +58,8 @@
             {:else}
             <Ellipse
                 x={x(d.cx)}
-                y={y(d.cy2)}
-                i={d.cy2}
+                y={y(d.cy)}
+                i={d.cy}
                 rx={rx(d.rminorR)}
                 ry={ry(d.rmajorR)}
                 fill='#F24968'
@@ -108,8 +67,8 @@
             />
             <Ellipse
                 x={x(d.cx)}
-                y={y(d.cy2)}
-                i={d.cy2}
+                y={y(d.cy)}
+                i={d.cy}
                 rx={rx(d.rminor)}
                 ry={ry(d.rmajor)}
                 fill='#18C2E6'
@@ -120,8 +79,3 @@
     </Canvas>
 
 </div>
-<style>
-    .overview {
-        height: 20vw;
-    }
-</style>
