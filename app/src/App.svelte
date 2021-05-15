f58e7ee3103e8d36cd0a6e572f62561d1e4ac756
xaquingv <xocasgv@gmail.com>
2021-05-15 18:01:18 +0200

    TWEAK Legend

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -14,29 +14,9 @@
 <main>
 	
 	{#each content as block}
-
-		{#if block.type === 'header'}
-		<Header {...block} />
+		{#if block.type === 'head'}
+		<Header {...block}/>
 		{:else if block.type === 'overview'}
-		<div class="legend">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 685.76 83.29">
-				<g style="isolation: isolate">
-				  <g>
-					<line y1="21.89" x2="674.49" y2="21.89" style="fill: none;stroke: #1ac1e5;stroke-miterlimit: 10;stroke-width: 2px"/>
-					<line y1="30.62" x2="674.49" y2="30.62" style="fill: none;stroke: #ef4a68;stroke-miterlimit: 10;stroke-width: 2px"/>
-					<text transform="translate(0 15.39)" style="font-size: 13px;fill: #231f20;font-family: OpenSans-Regular, Open Sans">This is people <tspan x="85.94" y="0" style="fill: #1ac1e5;font-family: OpenSans-Bold, Open Sans;font-weight: 700">asking for help</tspan><tspan x="183.33" y="0" xml:space="preserve"> on Google...</tspan></text>
-					<text transform="translate(0.99 45.75)" style="font-size: 13px;fill: #231f20;font-family: OpenSans-Regular, Open Sans">and this is is people <tspan x="122.76" y="0" style="fill: #ef4a68;font-family: OpenSans-Bold, Open Sans;font-weight: 700">offering help</tspan><tspan x="206.1" y="0" style="fill: #1bc1e5"> </tspan><tspan x="209.48" y="0">on Google over time</tspan></text>
-					<ellipse cx="428.15" cy="30.31" rx="15.47" ry="35.31" transform="translate(142.18 345.62) rotate(-52.27)" style="fill: #ef4968;mix-blend-mode: multiply"/>
-					<ellipse cx="428.15" cy="30.31" rx="35.31" ry="15.47" transform="translate(79.68 276.15) rotate(-39.77)" style="fill: #1bc1e5;mix-blend-mode: multiply"/>
-					<text transform="translate(469.15 48.56)" style="font-size: 13px;fill: #231f20;font-family: OpenSans-Regular, Open Sans">And this is a match: when people <tspan x="0" y="15.6">is asking for help and offering it </tspan><tspan x="0" y="31.2">at the same time for the same issue</tspan></text>
-					<g>
-					  <path d="M429.94,66.8c3.82,6.74,12.4,14.22,31.33,8.43" transform="translate(-0.01 -4.78)" style="fill: none;stroke: #3a3a3a;stroke-miterlimit: 10"/>
-					  <polygon points="428.09 64.84 427 53.67 433.53 62.8 428.09 64.84" style="fill: #3a3a3a"/>
-					</g>
-				  </g>
-				</g>
-			  </svg>  
-		</div>
 		<ScatterKnots {...block} data={oval_data}/>
 		{:else if block.type === 'text'}
 		<Text {...block} />
@@ -69,5 +49,8 @@
 		padding: 1em;
 		padding-bottom: 0;
 		margin: 0 auto;
+	}
+	section {
+		height: 90vh;
 	}
 </style>
