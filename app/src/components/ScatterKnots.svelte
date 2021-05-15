88869f5a473cb4eb2da42cdb24a41218732be1a7
carlo <czapponi@cisco.com>
2021-05-15 15:34:31 +0100

    mouse position for tooltip

--- app/src/components/ScatterKnots.svelte
+++ app/src/components/ScatterKnots.svelte
@@ -1,4 +1,4 @@
-  
+
 <script>
 	//import Axis from '../common/Axis.svelte';
 	//import Tooltip from '../common/Tooltip.svelte'
@@ -7,19 +7,32 @@
 	import {scaleSqrt, scaleLinear} from 'd3-scale';
 	import {extent} from 'd3-array';
     import Grid from './Grid.svelte'
-    
+
     export let data;
-    let margin = {top: 20, right: 10, bottom: 20, left: 110};
-	let width, height;
+    let margin = {top: 20, right: 10, bottom: 20, left: 40};
+	  let width, height;
+    let frame;
+
+
+    let dataMap = data.reduce((acc,d) => {
+      acc[d.cy - 1] = acc[d.cy - 1] ?? [];
+      acc[d.cy - 1][d.cx - 1] = d;
+      return acc;
+    }, [])
+
+    let tooltipPosition = [-1,-1];
 
     $: x = scaleLinear()
 		.domain(extent(data, d => d.cx))
 		.range([margin.left + 10, width - margin.right - margin.right]);
-	
+
+    $: stepX = (x.range()[1] - x.range()[0]) / dataMap[0].length;
+    $: stepY = (y.range()[1] - y.range()[0]) / dataMap.length;
+
 	$: y = scaleLinear()
 		.domain(extent(data, d => d.cy))
-        .range([margin.top + 20, height - margin.bottom - margin.top ]);
-        
+        .range([margin.top, height - margin.bottom - margin.top ]);
+
     $: rx = scaleSqrt()
 		.domain(extent(data, d => d.rminor))
 		.range([0, 5])
@@ -30,6 +43,34 @@
 		.range([0, 12])
 		.nice();
 
+    function updateTooltip(x,y) {
+      console.log(dataMap[y][x]);
+    }
+
+    function handleMousemove(event) {
+      const row = Math.min(Math.max(0, Math.floor((event.clientY - (margin.top)) / stepY)), dataMap.length - 1);
+      const col = Math.min(Math.max(0, Math.floor((event.clientX - (margin.left + 10)) / stepX)), dataMap[row].length - 1);
+
+      // console.log(col,row,'->');
+      // console.log(dataMap[row][col])
+      if(tooltipPosition.col !== col || tooltipPosition.row !== row) {
+        updateTooltip(col, row);
+      }
+
+  	}
+    // LET'S USE THIS LATER TO IMPROVE THE PERFORMANCE
+    function handleMouseenter() {
+      console.log('ENTER');
+      // (function loop() {
+  		// 	frame = requestAnimationFrame(loop);
+  		// 	// console.log(tooltipPosition);
+  		// })();
+    }
+    function handleMouseleave() {
+      console.log('LEAVE');
+      // cancelAnimationFrame(frame);
+    }
+
     const color = {
         "MH":{pink: '#F28095', blue: '#8CE6FF'},
         "H":{pink: '#F24968', blue: '#18C2E6'},
@@ -37,15 +78,15 @@
         "P":{pink: '#A60321', blue: '#1262A1'},
         "N":{pink: '#A60321', blue: '#1262A1'}
     }
-		
+
 </script>
 <div class='graphic overview' bind:clientWidth={width} bind:clientHeight={height}>
-    <Canvas {width} {height}>
+    <Canvas {width} {height} on:mousemove={handleMousemove} on:mouseenter={handleMouseenter} on:mouseleave={handleMouseleave}>
         <Grid type="x" scale={x} {data} {margin} />
         <Grid type="y" scale={y} {data} {margin} />
             {#each data as d,i}
             {#if i%2}
-            <Ellipse 
+            <Ellipse
                 x={x(d.cx)}
                 y={y(d.cy)}
                 i={d.cy}
@@ -54,7 +95,7 @@
                 fill={color[d.type].pink}
                 rotation={Math.PI * .25}
             />
-            <Ellipse 
+            <Ellipse
                 x={x(d.cx)}
                 y={y(d.cy)}
                 i={d.cy}
@@ -64,7 +105,7 @@
                 rotation={Math.PI * .75}
             />
             {:else}
-            <Ellipse 
+            <Ellipse
                 x={x(d.cx)}
                 y={y(d.cy)}
                 i={d.cy}
@@ -73,7 +114,7 @@
                 fill={color[d.type].pink}
                 rotation={Math.PI * .25}
             />
-            <Ellipse 
+            <Ellipse
                 x={x(d.cx)}
                 y={y(d.cy)}
                 i={d.cy}
