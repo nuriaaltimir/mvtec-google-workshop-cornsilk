7e834554dd6cbbb55ac6e5cb81149732eca9c501
Spe Chen Yu-Syuan <spechen@sph.com.sg>
2021-05-17 20:57:59 +0800

    preprocessing data for blanket so to take one json only

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -5,16 +5,55 @@
 	import Text from './components/Text.svelte'
 	import Knit from './components/Knit.svelte'
 	import FullBlanket from './components/FullBlanket.svelte'
+	import Blanket from './components/Blanket.svelte'
 	import Footer from './components/common/Footer.svelte'
 
 	export let content;
 	export let oval_data;
-	export let oval_data2;
+	// export let oval_data2;
 
-
-	let selected = oval_data2.filter(d => d.name === "Depression")
+	let oval_data2 = oval_data.map(d => {return {
+        // cx:d.order, // time in months
+        cx:d.month, // time in months
+        // cy:d.index, // subjects ordered
+        cy: Math.ceil(d.cx/12), // subjects ordered
+        ////building the oval so that the value is the area
+        rmajor: d.rmajor, 
+        rminor: d.rminor,
+        rmajorR: d.rmajorR, 
+        rminorR: d.rminorR,
+        type:d.type, 
+		name:d.lable,  
+		value: d.value,
+		value2: d.value2,
+		year: d.year, 
+		month: d.month        
+        };}
+		);
+	let selected = oval_data2.filter(d => d.type === "MH")
 	let types = [...new Set(oval_data2.map((d) => d.name))];
 
+	// blanket data transform
+	// console.log(selected)
+	const uniqify = (array, key) => array.reduce((prev, curr) => prev.find(a => a[key] === curr[key]) ? prev : prev.push(curr) && prev, []);
+    const selectedTopics = uniqify(selected, 'name').map( d => d.name)
+  
+	
+	var i;
+    let dataNew = [];
+    for (i = 0; i < selectedTopics.length; i++) {
+		var innerObj = {};
+		innerObj['name'] = selectedTopics[i]
+        innerObj['values'] = selected.filter( d=>d.name === selectedTopics[i])
+        dataNew.push(innerObj)         
+    }
+	// console.log(dataNew[0]['values']);
+	console.log(selected.filter(d=>d.name === "Addiction"))
+	// blanket data check
+
+	console.log("==========")
+	console.log(oval_data)
+	console.log(oval_data2)
 
 </script>
 
@@ -29,10 +68,11 @@
 		<Text {...block} />
 		{:else if block.type === 'chart'}
 		<Knit {...block} />
+		<!-- <Blanket data={selected}/> -->
 		{:else if block.type === 'blanket'}
 		<div class = "custom-select"> 
 		</div>	
-		<FullBlanket data={selected}/>
+		<FullBlanket data={dataNew}/>
 		{:else if block.type === 'footer'}
 		<Footer>
 			<div slot="about">
