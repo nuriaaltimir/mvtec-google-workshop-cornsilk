ba3becfa0e95b74aaaeeb1f094487bfb84d9505b
laMonre <depaso@gmail.com>
2021-05-20 17:18:58 +0200

    Knit.svelte component between the text

---
+++ app/src/components/Knit.svelte
@@ -0,0 +1,28 @@
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="694.33px" height="74.15px" viewBox="0 0 694.33 74.15" style="overflow:visible;enable-background:new 0 0 694.33 74.15;"
+	 xml:space="preserve">
+<style type="text/css">
+	.st0{fill:none;stroke:#26B0E6;stroke-width:4;stroke-miterlimit:10;}
+	.st1{fill:none;stroke:#F16680;stroke-width:4;stroke-miterlimit:10;}
+	.st2{fill:none;stroke:#3A3A3A;stroke-miterlimit:10;}
+	.st3{fill:#3A3A3A;}
+	.st4{fill:#26B0E6;}
+	.st5{fill:#F16680;}
+</style>
+<defs>
+</defs>
+<line class="st0" x1="0" y1="30.09" x2="694.33" y2="30.09"/>
+<line class="st1" x1="0" y1="35.82" x2="694.33" y2="35.82"/>
+<g>
+	<g>
+		<path class="st2" d="M345.96,55.97c1.07,5.01,6.97,23.89,34.05,15.6"/>
+		<g>
+			<path class="st3" d="M342.51,63.57c0.26,0.1,0.55-0.02,0.65-0.28l2.85-7.1l5.23,5.59c0.19,0.2,0.51,0.21,0.71,0.02
+				c0.2-0.19,0.21-0.51,0.02-0.71l-5.76-6.16c-0.12-0.12-0.29-0.18-0.46-0.15s-0.31,0.15-0.37,0.31l-3.14,7.83
+				c-0.04,0.09-0.04,0.19-0.03,0.28C342.24,63.36,342.35,63.5,342.51,63.57z"/>
+		</g>
+	</g>
+</g>
+<ellipse transform="matrix(0.7071 -0.7071 0.7071 0.7071 82.5754 252.2265)" class="st5" cx="345.75" cy="33.44" rx="14.79" ry="34.33"/>
+<ellipse transform="matrix(0.7071 -0.7071 0.7071 0.7071 82.5754 252.2265)" class="st4" cx="345.75" cy="33.44" rx="34.33" ry="14.79"/>
+</svg>
