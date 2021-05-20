11764c9bb932934ee9deef0f03358738c95c0c0c
larago <la.arago@gmail.com>
2021-05-20 15:58:26 +0200

    chang styles

---
+++ app/src/components/LegendMobile.svelte
@@ -0,0 +1,17 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="363.09" height="139.91" viewBox="0 0 363.09 139.91">
+    <g style="isolation: isolate">
+      <g>
+        <line y1="34.53" x2="363.09" y2="34.53" style="fill: none;stroke: #1ac1e5;stroke-miterlimit: 10;stroke-width: 2px"/>
+        <line y1="43.27" x2="363.09" y2="43.27" style="fill: none;stroke: #ef4a68;stroke-miterlimit: 10;stroke-width: 2px"/>
+        <text transform="translate(0.11 11.14)" style="font-size: 13px;fill: #231f20;font-family: OpenSans-Regular, Open Sans">This thread is people<tspan x="0" y="15.6" xml:space="preserve"> in </tspan><tspan x="18.02" y="15.6" style="fill: #1ac1e5;font-family: OpenSans-Bold, Open Sans;font-weight: 700">need of help</tspan></text>
+        <text transform="translate(0.99 58.39)" style="font-size: 13px;fill: #231f20;font-family: OpenSans-Regular, Open Sans">and this people seeking <tspan x="0" y="15.6">to help someone on </tspan><tspan x="0" y="31.2">Google over time</tspan></text>
+        <ellipse cx="617.47" cy="305.54" rx="15.47" ry="35.31" transform="translate(-471.01 198.38) rotate(-39.77)" style="fill: #ef4968;mix-blend-mode: multiply"/>
+        <ellipse cx="617.47" cy="305.54" rx="35.31" ry="15.47" transform="translate(-420.46 339.59) rotate(-52.27)" style="fill: #1bc1e5;mix-blend-mode: multiply"/>
+        <text transform="translate(232.99 58.39)" style="font-size: 13px;fill: #231f20;font-family: OpenSans-Regular, Open Sans">Where they meet<tspan x="0" y="15.6">in a big knot, people</tspan><tspan x="0" y="31.2">are asking for help</tspan><tspan x="0" y="46.8">and offering it at the</tspan><tspan x="0" y="62.4">same time for the </tspan><tspan x="0" y="78">same issue</tspan></text>
+        <g>
+          <path d="M610.79,346c3.81,6.74,12.39,14.22,31.32,8.42" transform="translate(-418.44 -267.36)" style="fill: none;stroke: #3a3a3a;stroke-miterlimit: 10"/>
+          <polygon points="190.51 81.45 189.41 70.28 195.94 79.41 190.51 81.45" style="fill: #3a3a3a"/>
+        </g>
+      </g>
+    </g>
+  </svg>
