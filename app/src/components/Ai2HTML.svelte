74787f3d85036fd5e32d57e8512ad93b06880d19
xaquingv <xocasgv@gmail.com>
2021-05-20 17:22:20 +0200

    Create Ai2HTML.svelte

---
+++ app/src/components/Ai2HTML.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let file;
+	$:html = '';
+    
+	fetch(`./${file}`)
+        .then(resp => resp.text())
+        .then(text => { html = text })
+    
+</script>
+
+{@html html}
