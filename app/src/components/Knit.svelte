b27b4c3d0b4b31849ea5d5b275ec87e74635b60b
Spe Chen Yu-Syuan <spechen@sph.com.sg>
2021-05-21 02:16:23 +0800

    blanket changes

---
+++ app/src/components/Knit.svelte
@@ -0,0 +1,24 @@
+<script>
+    import rough from "roughjs/bin/rough";
+    // npm i roughjs@4.0.4
+    // https://roughjs.com/
+    let roughSvg;
+    
+    function action(node) {
+      roughSvg = rough.svg(node); 
+      const rect = roughSvg.rectangle(100, 50, 100, 50, {  // x, y, width, height
+        fill: "red"
+      });
+      node.appendChild(rect);
+    }
+  </script>
+  
+  <svg use:action />
+  
+  <style>
+    svg {
+      width: 300px;
+      height: 150px;
+      background-color: white;
+    }
+  </style>
