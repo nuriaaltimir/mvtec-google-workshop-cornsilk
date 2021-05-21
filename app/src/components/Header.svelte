02ec4198015937f362f199d3384a612ccd43ef8e
larago <la.arago@gmail.com>
2021-05-21 17:52:31 +0200

    fixe legend

--- app/src/components/Header.svelte
+++ app/src/components/Header.svelte
@@ -13,13 +13,13 @@
     </h1> 
     <h2>{subhead}</h2>
     <div class='legend'>
-      <p style='top:0; left: 0; width:90%' class='label'>This thread is <span class='text-blue'>people in need of help</span> ...</p>
-      <p style='top:40%; left: 0; width:60%' class='label'>... and this one is <span class='text-red'>people seeking to help someone</span> on Google, over time.</p>
+      <p style='top:0; left: 0; width:90%' class='label'>This thread is <span class='text-red'>people in need of help</span> ...</p>
+      <p style='top:40%; left: 0; width:60%' class='label'>... and this one is <span class='text-blue'>people seeking to help someone</span> on Google, over time.</p>
       <p style='top:40%; left: 90%; width:60%' class='label'>Where <span class='text-black'>they meet in a big knot</span>, people are asking for help and offering it at the same month for the same issue</p>
       <Legend/></div>
     <div class='legend_mobile'>
-      <p style='top:-10%; left: 0; width:60%; line-height: 20px;' class='label'>This thread is people <span class='text-blue'>in need of help</span> ...</p>
-      <p style='top:46%; left: 0; width:70%' class='label'>... and this one is <br><span class='text-red'>people seeking to <br>help someone</span> <br>on Google, over time.</p>
+      <p style='top:-10%; left: 0; width:60%; line-height: 20px;' class='label'>This thread is people <span class='text-red'>in need of help</span> ...</p>
+      <p style='top:46%; left: 0; width:70%' class='label'>... and this one is <br><span class='text-blue'>people seeking to <br>help someone</span> <br>on Google, over time.</p>
       <p style='top:46%; left: 65%; width:50%' class='label'>Where <span class='text-black'>they meet in a big knot</span>, people are asking for help and offering it at the same quarter for the same issue</p>
       <LegendMobile/></div>
     
